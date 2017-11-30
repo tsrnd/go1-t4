6da44c1f133afd3ca167af8113659a9ce284b8fe
at-hungnguyen <hung.nguyen@asiantech.vn>
2017-11-30 17:37:34 +0700

    Update template admin + Use it

--- models/productGroup.go
+++ models/productGroup.go
@@ -1,10 +1,25 @@
 package models
 
 import (
+	"log"
 	"github.com/jinzhu/gorm"
+	"github.com/goweb4/database"
 )
 
 type ProductGroup struct {
 	gorm.Model	
 	Name    string		`schema:"name"`
+}
+
+func GetProductGroups() (productGroups []ProductGroup, err error) {
+	productGroups = []ProductGroup{}
+	db, errConnection := database.DBConnection(); if errConnection != nil {
+		log.Fatal(errConnection)
+		return productGroups, errConnection
+	}
+	defer db.Close()
+
+	err = db.Find(&productGroups).Error
+
+	return productGroups, err
 }
