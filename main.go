ee79ba16996e601120e5c7fb8ffbf348a72b62d6
Hung Nguyen V <hung.nguyen@asiantech.vn>
2017-12-05 15:29:00 +0700

    Tmp

--- main.go
+++ main.go
@@ -4,11 +4,15 @@ import (
 	"log"
 	"net/http"
 	"os"
-
+	"github.com/goweb4/database"
 	"github.com/goweb4/config"
 )
 
 func main() {
+	database.DBCon, errConnection = database.DBConnection(); if errConnection != nil {
+		log.Fatal(errConnection)
+	}
+	defer database.DBCon.close()
 	router := config.NewRouter()
 	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), router))
 }
