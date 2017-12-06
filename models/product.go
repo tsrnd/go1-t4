d14bab719d343573f2572576916c33bcdce73764
Vien Dinh Q <vien.dinh@asiantech.vn>
2017-12-06 14:21:50 +0700

    best seller product

--- models/product.go
+++ models/product.go
@@ -1,41 +1,46 @@
 package models
 
 import (
+	"fmt"
+
 	"github.com/goweb4/database"
 	"github.com/jinzhu/gorm"
 )
 
 type Product struct {
 	gorm.Model
-	Size    		string  		`schema:"size"`
-	Color   		string  		`schema:"color"`
-	Price   		float64 		`schema:"price"`
-	Name    		string  		`schema:"name"`
-	InStock 		uint    		`schema:"in_stock"`
-	GroupID 		uint    		`schema:"group_id"`
-	ProductGroup	ProductGroup	`gorm:"ForeignKey:GroupId"`//belong To Product Group
-	OrderProducts	[]OrderProduct	//has many order products
-	Images			[]Image			//has many image
+	Size          string         `schema:"size"`
+	Color         string         `schema:"color"`
+	Price         float64        `schema:"price"`
+	Name          string         `schema:"name"`
+	InStock       uint           `schema:"in_stock"`
+	GroupID       uint           `schema:"group_id"`
+	ProductGroup  ProductGroup   `gorm:"ForeignKey:GroupId"` //belong To Product Group
+	OrderProducts []OrderProduct //has many order products
+	Images        []Image        //has many image
 }
 
-func (product *Product) GetRelationship() map[string]interface{}{
-	relationship := map[string]interface{} {
-		"Images": &product.Images,
-		"ProductGroup": &product.ProductGroup,
+func (product *Product) GetRelationship() map[string]interface{} {
+	relationship := map[string]interface{}{
+		"Images":        &product.Images,
+		"ProductGroup":  &product.ProductGroup,
 		"OrderProducts": &product.OrderProducts,
 	}
 	return relationship
 }
 
-func GetProducts() (products []Product, err error) {
-	err = database.DBCon.Find(&products).Error
-	return products, err
+func GetProducts() (products []Product) {
+	err := database.DBCon.Find(&products).Error
+	if err != nil {
+		return products
+	}
+	return products
 }
 
 func GetProduct(id uint) (product Product, err error) {
 	err = database.DBCon.Where("id = ?", id).Find(&product).Error
 	database.DBCon.Model(&product).Association("Images").Find(&product.Images)
-	
+
 	return product, err
 }
 
@@ -59,4 +64,27 @@ func CreateProduct(product *Product) (proId uint, err error) {
 		proId = product.ID
 	}
 	return proId, err
+}
+func GetTrendProducts() (listProduct []Product) {
+
+	rows, err := database.DBCon.Table("order_products").
+		Select("product_id, sum(quantity) as total").
+		Group("product_id").
+		Order("total desc").
+		Limit(4).
+		Rows()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	for rows.Next() {
+		var id, quantity uint
+		product := Product{}
+		if err := rows.Scan(&id, &quantity); err != nil {
+			fmt.Println(err)
+		}
+		database.DBCon.Where("id = ?", id).First(&product)
+		listProduct = append(listProduct, product)
+	}
+	return listProduct
 }
