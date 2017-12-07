aa59fa5c3a56ed48741250db63510c4a6c87ba4a
Hung Nguyen V <hung.nguyen@asiantech.vn>
2017-12-07 15:38:24 +0700

    Add Filter User

--- routers/router.go
+++ routers/router.go
@@ -1,8 +1,10 @@
 package routers
 
 import (
+	"strings"
 	"github.com/astaxie/beego"
 	"github.com/goweb4/controllers"
+	ctx "github.com/astaxie/beego/context"
 )
 
 type Route struct {
@@ -30,7 +32,22 @@ var routes = Routes{
 	},
 }
 
+var FilterUser = func(ctx *ctx.Context) {
+	if strings.HasPrefix(ctx.Input.URL(), "/login") {
+		return
+	}
+
+	_, ok := ctx.Input.Session("uid").(int)
+	if !ok {
+			ctx.Redirect(302, "/login")
+	}
+}
+
 func init() {
+	if (beego.AppConfig.String("FilterUser") == "true") {
+		beego.InsertFilter("/checkout", beego.BeforeRouter, FilterUser)
+		beego.InsertFilter("/profile", beego.BeforeRouter, FilterUser)
+	}
 	for _, route := range routes {
 		beego.Router(route.URL, route.Handler, route.mappingMethod...)
 	}
