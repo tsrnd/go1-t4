3f0c0f1dcc820314aaee39790f9ba1f52f980ee4
Huyen Nguyen T.L <huyen.nguyen@asiantech.vn>
2017-12-11 18:26:33 +0700

    fix get payment

--- models/payment.go
+++ models/payment.go
@@ -1,15 +1,25 @@
 package models
 
-import (
-	
-)
+import "github.com/goweb4/database"
 
 type Payment struct {
 	Model
 	Method string
 }
 
-// func GetPayments() (payments []Payment, err error) {
-// 	err = database.DBCon.Find(&payments).Error
-// 	return payments, err
-// }
+func GetPayments() (payments []Payment, err error) {
+	rows, err := database.DBCon.Query("SELECT method FROM payments")
+	if err != nil {
+		return
+	}
+	for rows.Next() {
+		payment := Payment{}
+		err = rows.Scan(&payment.Method)
+		if err != nil {
+			return
+		}
+		payments = append(payments, payment)
+	}
+	rows.Close()
+	return payments, nil
+}
