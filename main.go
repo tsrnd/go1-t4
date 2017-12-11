a5a429635f20d30ffb08df45e768a58fb211a348
Huyen Nguyen T.L <huyen.nguyen@asiantech.vn>
2017-12-11 13:59:52 +0700

    delete env file

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"log"
 	"net/http"
-	"os"
 
 	"github.com/goweb4/config"
 	"github.com/goweb4/database"
@@ -18,5 +17,5 @@ func main() {
 	}
 	defer database.DBCon.Close()
 	router := config.NewRouter()
-	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), router))
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
