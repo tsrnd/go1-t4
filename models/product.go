db1e064ba47532e1f2101a11311b5798fef76d82
Vien Dinh Q <vien.dinh@asiantech.vn>
2017-12-13 10:15:57 +0700

    get latest products

--- models/product.go
+++ models/product.go
@@ -17,8 +17,8 @@ type Product struct {
 	Images        []Image        //has many image
 }
 
-func (product *Product) GetSchema() ([]interface{}) {
-	return []interface{} {
+func (product *Product) GetSchema() []interface{} {
+	return []interface{}{
 		&product.ID,
 		&product.Size,
 		&product.Color,
@@ -32,7 +32,7 @@ func (product *Product) GetSchema() ([]interface{}) {
 	}
 }
 
-func (product *Product) TableName() (string) {
+func (product *Product) TableName() string {
 	return "products"
 }
 
@@ -49,11 +49,11 @@ func (product *Product) Test() {
 // }
 
 // func GetProducts() (products []Product) {
-	// err := database.DBCon.Find(&products).Error
-	// if err != nil {
-	// 	return products
-	// }
-	// return products
+// err := database.DBCon.Find(&products).Error
+// if err != nil {
+// 	return products
+// }
+// return products
 // }
 
 func GetProduct(id uint) (product Product, err error) {
@@ -107,10 +107,18 @@ func GetTrendProducts() (listProduct []Product) {
 	return listProduct
 }
 
-func GetLatestProduct() (products []Product) {
-	// err := database.DBCon.Last(&products).Limit(4).Find(&products).Error
-	// if err != nil {
-	// 	return products
-	// }
+func GetLatestProduct(limit int) (products []Product) {
+	rows, err := database.DBCon.Db.Query("SELECT id, size, name, price from products ORDER BY id DESC limit $1", limit)
+	if err != nil {
+		return
+	}
+	for rows.Next() {
+		product := Product{}
+		err = rows.Scan(&product.ID, &product.Size, &product.Name, &product.Price)
+		if err != nil {
+			return
+		}
+		products = append(products, product)
+	}
 	return products
 }
