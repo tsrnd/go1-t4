60d2e24f1c84d98ddd91eb53cc1806992a8c49e3
Hung Nguyen V <hung.nguyen@asiantech.vn>
2017-12-18 11:41:27 +0700

    Add Gift Repository Test

--- gift/repository/gift_repository_test.go
+++ gift/repository/gift_repository_test.go
@@ -1,1 +1,20 @@
 package repository
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/golang/mock/gomock"
+
+	mockRepos "github.com/goweb4/gift/repository/mock"
+)
+
+func TestGetByFromUserID(t *testing.T) {
+	// gifts := []gift.Gift{}
+	mockCtrl := gomock.NewController(t)
+	mockRepo := mockRepos.NewMockGiftRepository(mockCtrl)
+	mockRepo.EXPECT().GetByFromUserID(1).Return(nil, nil)
+	_, err := mockRepo.GetByFromUserID(1)
+	assert.Equal(t, nil, err)
+}
