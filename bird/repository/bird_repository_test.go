2fff9117cfab80dd233c464048e8fc33c250e6c6
at-namnguyen <nam.nguyen@asiantech.vn>
2017-12-18 16:36:01 +0700

    repository mock and test

--- bird/repository/bird_repository_test.go
+++ bird/repository/bird_repository_test.go
@@ -1,1 +1,33 @@
-package repository_test
+package repository
+
+import (
+	"testing"
+	"github.com/golang/mock/gomock"
+	"github.com/goweb4/bird/repository/mocks"
+	"github.com/stretchr/testify/assert"
+	models "github.com/goweb4/bird"
+)
+
+func TestBirdCreateSuccess(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	mockRepo := mocks.NewMockBirdRepository(mockCtrl)
+	mockRepo.EXPECT().Create("My bird", "blue", "blue bird").Return(int64(1), nil).Times(1)
+	_, err := mockRepo.Create("My bird", "blue", "blue bird")
+	assert.Equal(t, nil, err)
+}
+
+func TestBirdCreateErrors(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	var expectData *models.Bird
+
+	mockRepo := mocks.NewMockBirdRepository(mockCtrl)
+	mockRepo.EXPECT().GetByID(int64(1)).Return(expectData, nil).Times(1)
+
+	rs, err := mockRepo.GetByID(int64(1))
+	assert.Equal(t, nil, err)
+	assert.NotEqual(t, rs, nil)
+}		
