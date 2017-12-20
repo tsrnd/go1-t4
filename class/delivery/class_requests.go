4aa599e4b1310cff22c6971d8774b292041864a5
Huyen Nguyen T.L <huyen.nguyen@asiantech.vn>
2017-12-20 08:38:29 +0700

    change code in class_repository_test and class_usecase_test

--- class/delivery/class_requests.go
+++ class/delivery/class_requests.go
@@ -4,3 +4,8 @@ type ClassCreationRequest struct {
 	Name          string `json:"name"`
 	StudentNumber int    `json:"student_number"`
 }
+
+type ClassLoginRequest struct {
+	Name string `json:"name"`
+	Pass string `json:"pass"`
+}
